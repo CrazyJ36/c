32935f38df48959cdacc49e87e61eb365121ddea
CrazyJ36 <crazy_j36@hotmail.com>
2018-05-21 21:01:35 -0400

    improve switch-statement

--- switch-statement.c
+++ switch-statement.c
@@ -2,7 +2,19 @@
    of an expression with a constant 'case' value.
    The switch statement often provides A more elegant solution
    to nested if statements.
-
+   'break;' must be used to end the switch statement at needed parts.
+   The Expression must be one that can be expressed as integral(int or char).
+   There may be multiple cases before statements are executed, like
+   nested if statements:
+   switch (expression) {
+     case 1:
+     case 2:
+       printf("expression equals case 1 or 2\n");
+       break;
+     default:
+       printf("Other\n");
+       break;
+   }
 */
 
 #include <stdio.h>
