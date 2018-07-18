3d82841cd758b24ddc0046ef932c48cf61d03452
CrazyJ36 <crazy_j36@hotmail.com>
2018-07-18 04:41:36 -0400

    better suspend program example

--- suspend-program.c
+++ suspend-program.c
@@ -8,11 +8,19 @@
 
 #include <stdio.h>
 #include <signal.h>
+#include <stdlib.h>
 
 int main() {
-  raise(SIGTSTP); // (sigint 17,19,23)
+  printf("going to background to do some task that don't exist.\n");
+  printf("you can type 'ps' to see this process is still running.\n");
+  printf("simply type 'fg' to get me back..\n");
+
+  raise(SIGSTOP); // (sigint 17,19,23)
   // at this point if i do sigcont, i can continue working,
   //   or print confirmations that it is back.
+
+  printf("'suspend-program' is back. exiting..\n");
+  exit(0);
 }
 
 
--- todo/get-window-size-change.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-  if (SIGWINCH) {
-    printf("Windows Size Changed @CrazyJ36\n");
