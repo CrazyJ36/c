ced773ced5685cf88310e97d2dbcb148a5052336
CrazyJ36 <crazy_j36@hotmail.com>
2018-09-03 01:59:56 -0400

    u

--- escape-sequence-characters.c
+++ escape-sequence-characters.c
@@ -28,16 +28,18 @@
       ??>  -  }
       ??-  -  ~
 */
+#include <stdio.h>
+
 int main()
 {
   printf("\v"); // simply prints vertical tab (4 new lines).
 
-printf("Examples of escape characters and how to 'escape' them\n\n");
+  printf("Examples of escape characters and how to 'escape' them\n\n");
 
- printf("The percent sign(%%) usually is used to insert A variable into a printf charsequence.\n");
- printf("To use it as text, it must be escaped in such A way that C knows\n");
- printf("the first percent sign you type is not used as A placeholder.\n");
- printf("You should type %%%%(two percents) in order to use a percent symbol as text:\n");
- printf("output: %%\n");
- return(0);
+  printf("The percent sign(%%) usually is used to insert A variable into a printf charsequence.\n");
+  printf("To use it as text, it must be escaped in such A way that C knows\n");
+  printf("the first percent sign you type is not used as A placeholder.\n");
+  printf("You should type %%%%(two percents) in order to use a percent symbol as text:\n");
+  printf("output: %%\n");
+  return(0);
 }
