ef324f9dce599c37d39a5a119d9cf2a53b492d53
CrazyJ36 <crazy_j36@hotmail.com>
2018-09-04 09:20:41 -0400

    u

--- curses-fnkeys-game.c
+++ curses-fnkeys-game.c
@@ -1,38 +1,40 @@
 #include <ncurses.h>
 
 int main() {
-  initscr();
+  initscr(); // start curses
 
-  /* Enables use of arrow keys(important), fn keys(f1, f2...).
+  /* Enables use of arrow keys(important), fn
+     keys(f1, f2...).
      if this is not set, the program would end. */
   keypad(stdscr, TRUE);
 
-  printw("Type an arrow key.\n");
+  printw("Type an arrow key.\n"); // rules of 'game'
 
-  refresh();
+  refresh(); // refresh screen to show instructions
 
-  int ch;
-  int left = 0;
-  int right = 0;
-  int x;
-  for (x = 0; x < 10; x++)  {
-    ch = getch();
+  int ch; // to-be user input character
+  int left = 0; // player 1
+  int right = 0; // player 2
+  int x; // increment number for time limit
+  for (x = 0; x < 10; x++)  { // increment number for time limit
+    ch = getch(); // get device input
 
-    switch(ch) {
+    switch(ch) { // get left or right button as 'player turn'
       case KEY_LEFT:
-        left = left + 1;
-        break;
+        left = left + 1; // increase player score
+        break; /* either this(left) or right. either or, should break to return which one happened (like latch vs momentary). switch will loop again to get next 'turn'. */
       case KEY_RIGHT:
         right = right + 1;
         break;
    }
 
+    //print result each time
     printw("left: %d, right: %d\n", left, right);
     refresh();
   }
 
+  // after loop endwin()
   endwin();
   printf("done\n");
-
   return(0);
 }
