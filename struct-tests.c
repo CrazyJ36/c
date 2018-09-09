73757dc8998c021403ee7328def8eeefcb16d666
CrazyJ36 <crazy_j36@hotmail.com>
2018-09-09 02:47:45 -0400

    start struct tests

---
+++ struct-tests.c
@@ -0,0 +1,16 @@
+/* Structures (also the composite , aggregate data type. Also called records)
+   is a user-defined data type, grouping related variable members
+   with different data types if desired(strings, arrays, pointers, int, char...)
+
+*/
+#include <stdio.h>
+
+int main() {
+
+  struct names { // type, tag, { members; };
+    char[] jen;
+    char[] james;
+    int people;
+  }; // don't forget ';'
+
+}
