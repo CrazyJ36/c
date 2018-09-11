c6593f6ad5b71b5cae06cd826f5a22ea4a1c4d9b
CrazyJ36 <crazy_j36@hotmail.com>
2018-09-11 04:48:49 -0400

    print char multiple times, base eat-letters

---
+++ print-char-multiple.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+  // print char multiple times
+  char c = '*';
+  int count = 10;
+  for (int i = 0; i < count; i ++) {
+    putchar(c);
+  }
+  printf("\n");
+
+}
