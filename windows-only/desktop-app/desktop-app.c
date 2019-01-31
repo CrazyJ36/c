d0cbcc25d689d69e574198e92018a31ad7eb7458
CrazyJ36 <crazy_j36@hotmail.com>
2019-01-30 23:03:06 -0500

    windows desktop-app: strings

--- windows-only/desktop-app/desktop-app.c
+++ windows-only/desktop-app/desktop-app.c
@@ -1,12 +1,25 @@
 #include <windows.h>
 
+/* Strings in '#define': you don't have to count needed characters, concatenates
+   at compile time, can still use functions like sizeOf(MSG).
+*/
+#define MSG "Important message:\nNot really."
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
     LPSTR lpCmdLine, int nCmdShow)
 {
-	// MB_YESNOCANCEL shows three buttons(yes,no,cancel)
-	// MB_DEFBUTTON2 mean second option(no) is the default option
-	// MB_ICONQUESTION states the type of dialog(different icons and alert sounds): MB_ICON[WARNING,QUESTION,EXCLAMATION,ASTERISK]
-    // MessageBox can return IDOK,IDNO,IDCANCEL,IDABORT,IDRETRY,IDIGNORE. These have simple numbers assigned to them as well.
-	MessageBox(NULL, "Done", "Title", MB_YESNOCANCEL | MB_DEFBUTTON2 | MB_ICONQUESTION);
-	return(0);
+	/* MB_YESNOCANCEL shows three buttons(yes,no,cancel)
+	  MB_DEFBUTTON2 mean second option(no) is the default option
+	  MB_ICONQUESTION states the type of dialog(different icons and alert sounds): 
+	  MB_ICON[WARNING,QUESTION,EXCLAMATION,ASTERISK]
+      MessageBox can return IDOK,IDNO,IDCANCEL,IDABORT,IDRETRY,IDIGNORE.
+	  These have simple numbers assigned to them as well.
+	*/
+	
+	// Defining title string
+    char title[6] = "Hello";
+	
+	MessageBox(NULL, MSG, title, MB_YESNOCANCEL | MB_DEFBUTTON2 | MB_ICONQUESTION);
+	
+	exit(0);
 }
