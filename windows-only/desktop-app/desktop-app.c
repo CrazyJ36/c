05aaa1da014e0c0121b6cc723c80a177227b183b
CrazyJ36 <crazy_j36@hotmail.com>
2019-01-31 23:11:47 -0500

    move some windows c doc comments to /docs/c

--- windows-only/desktop-app/desktop-app.c
+++ windows-only/desktop-app/desktop-app.c
@@ -8,18 +8,7 @@
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
     LPSTR lpCmdLine, int nCmdShow)
 {
-	/* MB_YESNOCANCEL shows three buttons(yes,no,cancel)
-	  MB_DEFBUTTON2 mean second option(no) is the default option
-	  MB_ICONQUESTION states the type of dialog(different icons and alert sounds): 
-	  MB_ICON[WARNING,QUESTION,EXCLAMATION,ASTERISK]
-      MessageBox can return IDOK,IDNO,IDCANCEL,IDABORT,IDRETRY,IDIGNORE.
-	  These have simple numbers assigned to them as well.
-	*/
-	
-	// Defining title string
     char title[6] = "Hello";
-	
 	MessageBox(NULL, MSG, title, MB_YESNOCANCEL | MB_DEFBUTTON2 | MB_ICONQUESTION);
-	
 	exit(0);
 }
