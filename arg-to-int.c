8db2ad5f7b27f88d3b2b218c542ef8a677f6e5fe
CrazyJ36 <crazy_j36@hotmail.com>
2019-04-28 01:53:46 -0400

    argument to int example

---
+++ arg-to-int.c
@@ -0,0 +1,15 @@
+// arg-to-int.c
+// Convert cli argument, which is char pointer, to long integer.
+#include <stdio.h>
+#include <stdlib.h> // for string-to-long(strtol), exit()
+
+int main(int argc, char *argv[])
+{
+	if (argc >= 2) {
+	  int converted = atoi(argv[1]); 
+	  printf("Converted to INT from char: %d\n", converted);	 
+	} else {
+      printf("You must enter a number argument at command line. Try again.\n");
+	}
+	exit(0);
+}
--- arguments-as-var.c
+++ arguments-as-var.c
@@ -3,6 +3,6 @@
 int main(int argc, char *argv[])
 {
 	char *arg = argv[1];
-	printf("arg 1 is: %s\n", arg);
+	printf("arg 1 is: %s\n", &arg);
 	return(0);
 }
