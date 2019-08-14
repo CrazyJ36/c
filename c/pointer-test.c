b6121cb2d7241e25185cd41fee21dee744af0a56
CrazyJ36 <crazy_j36@hotmail.com>
2019-08-15 00:58:21 +0100

    pointer address stuff

--- c/pointer-test.c
+++ c/pointer-test.c
@@ -13,5 +13,7 @@
 int main() {
 
   int *an_address; // (* point to) A pointer to the memory address of new int variable (no assinged value)
-  printf("%x\n", &an_address);  // prints the hexadecimal(%x) memory address of variable
+  printf("Hexadecimal form of address: %x\n", &an_address);  // prints the hexadecimal(%x) memory address of variable
+  printf("Pointer address: %p\n", &an_address);
+  return(0);
 }
