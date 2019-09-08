03a6c34a382e35ea21f63191b8af62a208988ab3
CrazyJ36 <crazy_j36@hotmail.com>
2019-09-08 03:58:14 +0100

    pi: fix leds

---
+++ c/pi/flash-23.c
@@ -0,0 +1,22 @@
+/* gcc -o flash-11 flash-11.c -l bcm2835 */
+#include <stdio.h>
+#include <bcm2835.h>
+#define PIN RPI_GPIO_P1_16
+int main()
+{
+  if(!bcm2835_init()) return(1); // attempt initialize library functions
+  bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP); // select pin and output, pin num is as ordered on board
+  int incr = 1;
+  printf("Flashing LED on bcm pin 22.\n");
+  while(incr < 6) {
+    printf("%d out of 5..\n", incr);
+    bcm2835_gpio_write(PIN, HIGH);
+    delay(200);
+    bcm2835_gpio_write(PIN, LOW);
+    if(incr == 5) break;
+    delay(800);
+    incr++;
+  }
+  printf("Done\n");
+  return(0);
+}
