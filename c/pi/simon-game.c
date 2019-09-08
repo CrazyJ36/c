d1dd0e2fcf01251d1315292bd8d1bea0bca46035
CrazyJ36 <crazy_j36@hotmail.com>
2019-09-08 11:15:49 +0100

    pi: halfway simon game

--- c/pi/simon-game.c
+++ c/pi/simon-game.c
@@ -1,19 +1,18 @@
 #include <bcm2835.h>
 #include <stdio.h>
-#include <stdlib.h> // for srand() number generator
+#include <stdlib.h>
 #include <unistd.h>
 
-  int led1 = 18;
-  int led2 = 4;
-  int led3 = 22;
-  int led4 = 23;
-  int btn1 = 17;
-  int btn2 = 27;
-  int btn3 = 24;
-  int btn4 = 5;
-
-  int out = BCM2835_GPIO_FSEL_OUTP;
-  int in = BCM2835_GPIO_FSEL_INPT;
+int led1 = 18;
+int led2 = 4;
+int led3 = 22;
+int led4 = 23;
+int btn1 = 17;
+int btn2 = 27;
+int btn3 = 24;
+int btn4 = 5;
+int out = BCM2835_GPIO_FSEL_OUTP;
+int in = BCM2835_GPIO_FSEL_INPT;
 
 int exit_game() {
   bcm2835_close();
@@ -21,38 +20,27 @@ int exit_game() {
 }
 
 int flash(int led) {
-  printf("Flashing an LED..\n");
-  sleep(2);
+  printf("Flashing an LED..\n\n");
+  sleep(1);
   bcm2835_gpio_write(led, HIGH);
-  sleep(3);
-  bcm2835_gpio_write(led, LOW);
   sleep(2);
-  return led;
+  bcm2835_gpio_write(led, LOW);
 }
 
 void detect_button(int btn) {
    bcm2835_gpio_set_pud(btn, BCM2835_GPIO_PUD_UP);
-
-   printf("Press the button that is in the same position of order with led...\n");
-   printf("You will be timed:\n");
+   printf("Press the button that is in the same left-to-right order...\n");
    int count = 0;
-   while(count < 50) {
-     if(bcm2835_gpio_lev(btn) == 0) {
-       printf("Correct button %d pushed!\n", btn);
-       return;
-     } else if (bcm2835_gpio_lev(btn) == 1) {
-         delay(500);
-         printf("%d out of 50\n", count);
-         count++;
-     }
-     //printf("You ran out of time. Exiting\n");
-     //exit_game();
+   while( bcm2835_gpio_lev(btn) == 1 ) {
+     delay(100);
+     count++;
    }
+   printf("Correct %d button pressed!\n", btn);
 }
+
 int main() {
-  // If bcm2835_init() fails, exit with error.
   if (!bcm2835_init()) {
-    printf("bcm2835 library failed to initialize. Exiting..\n");
+    printf("bcm2835 failed.\n");
     return(1);
   }
 
@@ -65,45 +53,19 @@ int main() {
   bcm2835_gpio_fsel(btn3, in);
   bcm2835_gpio_fsel(btn4, in);
 
-  // Set max numbers
-  const int order1[4] = {led1, led2, led3, led4};
-  //const int order2[4] = {led3, led2, led1, led4};
-  //const int order3[4] = {led2, led3, led4, led1};
-  //const int order4[4] = {led4, led1, led2, led3};
-  const int btn_order1[4] = {btn1, btn2, btn3, btn4};
-  //const int btn_order2[4] = {btn3, btn2, btn1, btn4};
-  //const int btn_order3[4] = {btn2, btn3, btn4, btn1};
-  //const int btn_order4[4] = {btn4, btn1, btn2, btn4};
+  int order1[4] = {led1, led2, led3, led4};
+  //int order2[4] = {led3, led2, led1, led4};
+  //int order3[4] = {led2, led3, led4, led1};
+  //int order4[4] = {led4, led1, led2, led3};
+  int btn_order1[4] = {btn1, btn2, btn3, btn4};
+  //int btn_order2[4] = {btn3, btn2, btn1, btn4};
+  //int btn_order3[4] = {btn2, btn3, btn4, btn1};
+  //int btn_order4[4] = {btn4, btn1, btn2, btn4};
 
-  //int get_led = 0;
-  //for(get_led = 0; get_led > 4; get_led++) {
-    /*switch ( flash(get_led) ) {
-      case 17:
-        detect_button(btn2);
-        break;
-      case 18:
-        detect_button(btn1);
-        break;
-      case 24:
-        detect_button(btn3);
-        break;
-      case 5:
-        detect_button(btn4);
-        break;
-      //default:
-        //printf("?\n");
-        //exit_game();
-    }
+  for (int i = 0; i < 4; i++) {
+    flash(order1[i]);
+    detect_button(btn_order1[i]);
   }
-*/
-    for (int i = 0; i < 4; i++) {
-      while ( flash(order1[i]) ) {
-        detect_button(btn_order1[i]);
-      }
-      printf("You finished! Exiting..\n");
-      exit_game();
-    }
-//}
 
 }
 
