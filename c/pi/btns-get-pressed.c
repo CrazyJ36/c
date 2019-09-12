7245901dbb6cde6e4edeb55eb082d808ddc71d38
CrazyJ36 <crazy_j36@hotmail.com>
2019-09-12 20:52:21 +0100

    pi: get btns pressed

--- c/pi/btns-get-pressed.c
+++ c/pi/btns-get-pressed.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 #include <bcm2835.h>
 
-// Initialize pins
-//int led1 = RPI_GPIO_P1_12; // this calls board number, gpio num is 18
-//btn1=17;btn2=27;btn3=24;btn4 =5;
-
-// initialize A function to use
-//void myFunc(char func_version);
-
 int main() {
   if (!bcm2835_init()) return 1;
 
@@ -15,16 +8,14 @@ int main() {
   int btns[4] = {17, 27, 24, 5};
 
   // set button-occupied gpios' as input buttons
-  for(int x = 0; x < sizeof(btns); x++) {
-    bcm2835_gpio_fsel(btns[x], BCM2835_GPIO_FSEL_INPT);
-    //if (x == 0) {
-    //} else {
-      bcm2835_gpio_set_pud(btns[x], BCM2835_GPIO_PUD_UP);
-    //}
+  for(int i = 0; i < 4; i++) {
+    bcm2835_gpio_fsel(btns[i], BCM2835_GPIO_FSEL_INPT);
+    bcm2835_gpio_set_pud(btns[i], BCM2835_GPIO_PUD_UP);
   }
 
   printf("Push any button. To exit, press ctrl-c.\n");
 
+  // This can get any button, also multiple buttons at one time.
   while (1) {
     if (bcm2835_gpio_lev(btns[0]) == 0) printf("Button 1 pushed.\n");
     if (bcm2835_gpio_lev(btns[1]) == 0) printf("Button 2 pushed.\n");
@@ -37,5 +28,3 @@ int main() {
   return 0;
 
 }
-
-//void myFunc(func_verion)
