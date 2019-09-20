c188f11dec004b5f2947aaac204e94c413245459
CrazyJ36 <crazy_j36@hotmail.com>
2019-09-19 20:37:03 -0400

    pi: finish btns-get-pressed

--- c/pi/btns-get-pressed.c
+++ c/pi/btns-get-pressed.c
@@ -3,22 +3,24 @@
 
 int main() {
   if (!bcm2835_init()) return 1;
-  const int btns[8] = {23, 27, 22, 25, 9, 5, 8};
+
+  int btns[8] = {23, 27, 22, 25, 9, 5, 8};
   for(int i = 0; i < 8; i++) {
     bcm2835_gpio_fsel(btns[i], BCM2835_GPIO_FSEL_INPT);
     bcm2835_gpio_set_pud(btns[i], BCM2835_GPIO_PUD_UP);
   }
-  printf("Push any button. To exit, press ctrl-c.\n");
-  int x = 0;
+
+  printf("Push any button. To exit, press 'ctrl-c'.\n");
+
   while (1) {
-    for( x; x < 8; x++) {
+    // 'for' loops through all buttons(on every while loop) to catch if any btn is pressed.
+    for(int x = 0; x <= 8; x++) {
       if (bcm2835_gpio_lev(btns[x]) == 0) {
         printf("Button %d pushed.\n", btns[x]);
       }
     }
-    delay(300);
+
+    delay(250);
   }
 
-  bcm2835_close();
-  return 0;
 }
