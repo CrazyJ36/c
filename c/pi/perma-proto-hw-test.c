2b9c1bf851e6512733c4c1350e75ca6267cbd12d
CrazyJ36 <crazy_j36@hotmail.com>
2019-10-20 03:43:01 +0100

    pi: perma-proto-test progress

--- c/pi/perma-proto-hw-test.c
+++ c/pi/perma-proto-hw-test.c
@@ -1,25 +1,63 @@
 #include <bcm2835.h>
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+int leds();
+int buzzer();
+
+int main(int argc, char *argv[]) {
   if (!bcm2835_init()) return(1);
+  printf("Type: 'leds', 'buttons', or 'buzzer' to run A test\n");
+  char choice[7];
+  scanf("%8s", choice);
 
-  struct leds {
-    int led1;
-    int led2;
-    int led3;
-    int led4;
-    int led5;
-    int led6;
-    int led7;
-    int led8;
-  };
+  int choice_leds = strcmp("leds", choice);
+  int choice_buttons = strcmp("buttons", choice);
+  int choice_buzzer = strcmp("buzzer", choice);
 
-  struct leds pins = {4, 18, 24, 11, 12, 7, 16, 6};
-
-  for (int inc = 0; inc < sizeOf(pins); inc++) {
-    printf("led1: %d\n", pins.[inc]);
+  if (choice_leds == 0) {
+    printf("Running test on leds...\n");
+    leds();
+  } else if (choice_buttons == 0) {
+    printf("Test buttons.. push any..\n");
+    //buttons();
+  } else if (choice_buzzer == 0) {
+    printf("Testing buzzer..\n");
+    buzzer();
+  } else {
+    printf("Improper choice, try again. Exiting...\n");
   }
 
+  bcm2835_close();
+  return 0;
+}
+
+int leds() {
+  int pins[] = {4, 18, 24, 11, 12, 7, 16, 6};
+
+  for (int inc = 0; inc < 8; inc++) {
+    bcm2835_gpio_fsel(pins[inc], BCM2835_GPIO_FSEL_OUTP);
+  }
+
+  for (int inc = 0; inc < 8; inc++) {
+    printf("pin %d\n", pins[inc]);
+    bcm2835_gpio_write(pins[inc], HIGH);
+    delay(200);
+    bcm2835_gpio_write(pins[inc], LOW);
+    delay(200);
+  }
+  printf("Done.\n");
+  return 0;
+}
+
+int buzzer() {
+  int pin = 2;
+  bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_OUTP);
+  for (int inc = 0; inc < 40; inc++) {
+    bcm2835_gpio_write(pin, HIGH);
+    delay(5);
+    bcm2835_gpio_write(pin, LOW);
+    delay(5);
+  }
   return 0;
 }
