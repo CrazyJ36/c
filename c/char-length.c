c6910ac954c614e562fb97dc7dbec5194573f445
CrazyJ36 <crazy_j36@hotmail.com>
2019-11-18 00:52:57 -0500

    build char-length

--- c/char-length.c
+++ c/char-length.c
@@ -3,10 +3,13 @@
 
 void main()
 {
-        // You could define the length of char array here as: 'x[5]'
-        // for A character array of 'apes'. But we'll find the length.. 
+        /* You could define the length of char array here as: 'x[5]'
+         * for A character array of 'apes'. But we'll find the length..
+         */
 	char x[] = "apes";
-        // strlen() returns an int: the char length of A string which is the paramater
+        /* strlen() returns an int: the char length of A string
+         *  which is the paramater
+         */
 	int y = strlen(x);
 	printf("x is %d chars\n", y);
 }
