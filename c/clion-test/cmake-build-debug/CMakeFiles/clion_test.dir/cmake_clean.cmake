2cb6ab373fce71500c248526608ed8fb509280bb
CrazyJ36 <crazy_j36@hotmail.com>
2019-11-28 13:44:56 -0500

    u

---
+++ c/clion-test/cmake-build-debug/CMakeFiles/clion_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clion_test.dir/main.c.obj"
+  "clion_test.exe"
+  "clion_test.exe.manifest"
+  "clion_test.lib"
+  "clion_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/clion_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
