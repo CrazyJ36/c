5acbd3101f489b146da1271e928bd7792a8cd413
CrazyJ36 <crazy_j36@hotmail.com>
2019-12-27 01:13:58 -0500

    c/nested-forloop: build for rpi zero w

--- c/nested-forloop.c
+++ c/nested-forloop.c
@@ -3,7 +3,7 @@
 int main() {
 
     for (int i = 0; i < 2; i++) {
-        for (int j =0; j < 10; j++) {
+        for (int j = 0; j < 10; j++) {
             printf("Nested loop is at: %d\n", j);
         }
     printf("First(main) loop is at: %d\n", i);
