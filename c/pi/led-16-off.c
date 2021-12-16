5f752455540801de250243fedfc8cab2b20a3d11
CrazyJ36 <crazy_j36@hotmail.com>
2021-12-16 05:23:43 +0000

    pi led

---
+++ c/pi/led-16-off.c
@@ -0,0 +1,7 @@
+#include <bcm2835.h>
+int main() {
+  if (!bcm2835_init()) return(1);
+  bcm2835_gpio_fsel(16, BCM2835_GPIO_FSEL_OUTP); // simple 22 represent Gpio format number, or RPI_GPIO_P1_15
+  bcm2835_gpio_clr(16); // clr= clear or off, sets software gpio number to low
+  return(0);
+}
