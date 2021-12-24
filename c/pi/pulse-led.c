b0265d68eba9b3a71a1cdcf38652537ab6b09eec
CrazyJ36 <crazy_j36@hotmail.com>
2021-12-24 04:21:50 -0500

    u

--- c/pi/pulse-led.c
+++ c/pi/pulse-led.c
@@ -9,18 +9,16 @@ int main() {
   }
   bcm2835_gpio_fsel(LED, BCM2835_GPIO_FSEL_OUTP);
   int i = 0;
-  // Creating A fake PWM signal to change LED brightness over time.
-  for(int x = 0; x < 50; x++) {
+  int x = 1;
+  // Change LED brightness by alternating on/off time over iterations.
+  for(int i = 0; i < 40; i++) {
     bcm2835_gpio_write(LED, HIGH);
-    if(i % 2 == 0) {
-      delay(i + 1);
-    }
+      delay(x - i);
     bcm2835_gpio_write(LED, LOW);
-    if (i % 2 == 0) { // get divisors of 2 (even number or iterations) to make light turn off less while it gets brighter.
-      delay(i + 1);
-    }
-    i = i + 1;
+      delay(x + 1);
+    x = x + 1;
   }
+
   bcm2835_close();
   printf("Done\n");
 }
