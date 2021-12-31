93fa161babe4660d9a79de827cb0aacb34b00907
CrazyJ36 <crazy_j36@hotmail.com>
2021-12-31 16:52:27 -0500

    windows: begin native port of avoid meteors

--- c/windows/gui/paint-on-key/paint-on-key.c
+++ c/windows/gui/paint-on-key/paint-on-key.c
@@ -6,6 +6,9 @@
 
 int x_point = 50;
 int y_point = 50;
+int enemy_x = 0;
+int enemy_y = 0;
+
 PAINTSTRUCT ps;
 HDC hdc;
 
@@ -16,10 +19,26 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
         case WM_DESTROY:
             PostQuitMessage(0); break;
 		case WM_PAINT:
-        PAINTSTRUCT ps;
-            hdc = BeginPaint(hWnd, &ps); 
+            hdc = BeginPaint(hWnd, &ps);
 			SetPixel(hdc, x_point, y_point, 0x00000000); // params: SetPixel(BeginPaint(hWnd, &ps), x_position, y_position, color))
-			EndPaint(hWnd, &ps);
+            for (int line_top = 6; line_top < 326; line_top++) {
+                InvalidateRect(hWnd, NULL, FALSE);
+                SetPixel(hdc, line_top, 5, 0x00000000);
+            }
+            for (int line_bottom = 6; line_bottom < 326; line_bottom++) {
+                InvalidateRect(hWnd, NULL, FALSE);
+                SetPixel(hdc, line_bottom, 83, 0x00000000);
+            }
+            for (int line_left = 5; line_left < 84; line_left++) {
+                InvalidateRect(hWnd, NULL, FALSE);
+                SetPixel(hdc, 5, line_left, 0x00000000);
+            }
+            for (int line_right = 5; line_right < 84; line_right++) {
+                InvalidateRect(hWnd, NULL, FALSE);
+                SetPixel(hdc, 326, line_right, 0x00000000);
+            }
+            EndPaint(hWnd, &ps);
+            return 0;
         default: return DefWindowProc(hWnd, uMsg, wParam, lParam);
     }
     return 0;
@@ -36,7 +55,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     wc.lpszClassName = mClassName;
     wc.hbrBackground = (HBRUSH) (COLOR_WINDOW);
     RegisterClassEx(&wc);
-    HWND hWnd = CreateWindowEx(0, mClassName, "Title", WS_OVERLAPPEDWINDOW, 300, 300, 512, 248, NULL, NULL, hInstance, NULL);
+    HWND hWnd = CreateWindowEx(0, mClassName, "Title", WS_OVERLAPPEDWINDOW, 740, 350, 348, 128, NULL, NULL, hInstance, NULL);
     if(hWnd == NULL) return 0;
     ShowWindow(hWnd, nCmdShow);
 
@@ -45,24 +64,32 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     while(GetMessage(&Msg, NULL, 0, 0)) {
         TranslateMessage(&Msg);
         DispatchMessage(&Msg);
-
-        // Get messages from keydown in the main loop.
-        switch (Msg.message) {
-            case WM_KEYDOWN:
-                if (GetAsyncKeyState(VK_LEFT) & 0x25) {
-                    x_point = x_point - 1;
-                    InvalidateRect(hWnd, NULL, TRUE);
-                    SetPixel(hdc, x_point, y_point, 0x00000000);
-                    UpdateWindow(hWnd);
-                }
-                if (GetAsyncKeyState(VK_RIGHT)) {
-                    x_point = x_point + 1;
-                    InvalidateRect(hWnd, NULL, TRUE);
-                    SetPixel(hdc, x_point, y_point, 0x00000000);
-                    UpdateWindow(hWnd);
-
-                }
+        // Get messages from keys in the main loop.
+        if (GetAsyncKeyState(VK_LEFT)) {
+            x_point = x_point - 1;
+            InvalidateRect(hWnd, NULL, TRUE);
+            SetPixel(hdc, x_point, y_point, 0x00000000);
+            UpdateWindow(hWnd);
         }
+        if (GetAsyncKeyState(VK_RIGHT)) {
+            x_point = x_point + 1;
+            InvalidateRect(hWnd, NULL, TRUE);
+            SetPixel(hdc, x_point, y_point, 0x00000000);
+            UpdateWindow(hWnd);
+        }
+        if (GetAsyncKeyState(VK_UP)) {
+            y_point = y_point - 1;
+            InvalidateRect(hWnd, NULL, TRUE);
+            SetPixel(hdc, x_point, y_point, 0x00000000);
+            UpdateWindow(hWnd);
+        }
+        if (GetAsyncKeyState(VK_DOWN)) {
+            y_point = y_point + 1;
+            InvalidateRect(hWnd, NULL, TRUE);
+            SetPixel(hdc, x_point, y_point, 0x00000000);
+            UpdateWindow(hWnd);
+        }
+        
     }
     return Msg.wParam;  // returns messages ONCE after while.
 
