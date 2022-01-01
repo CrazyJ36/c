8e8581db6ce199660a7b376d4c5c322bd87dd914
CrazyJ36 <crazy_j36@hotmail.com>
2021-12-31 20:41:43 -0500

    u

--- c/windows/gui/paint-on-key/paint-on-key.c
+++ c/windows/gui/paint-on-key/paint-on-key.c
@@ -1,5 +1,6 @@
 /* Build with: > cl program.c*/
 #include <windows.h>
+#include <stdio.h>
 
 #pragma comment(lib,"user32.lib") // for autolinking libs at compile time.
 #pragma comment(lib,"Gdi32.lib")
@@ -8,7 +9,8 @@ int x_point = 50;
 int y_point = 50;
 int enemy_x = 0;
 int enemy_y = 0;
-
+int color = 0x00000000;
+int enemy_color = 0x000000FF;
 PAINTSTRUCT ps;
 HDC hdc;
 
@@ -20,21 +22,29 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
             PostQuitMessage(0); break;
 		case WM_PAINT:
             hdc = BeginPaint(hWnd, &ps);
-			SetPixel(hdc, x_point, y_point, 0x00000000); // params: SetPixel(BeginPaint(hWnd, &ps), x_position, y_position, color))
+            // Must setup all needed points in WM_PAINT, or they don't get created in WinMain. 
+            SetPixel(hdc, x_point, y_point, color); // params: SetPixel(BeginPaint(hWnd, &ps), x_position, y_position, color))
+            SetPixel(hdc, x_point + 1, y_point + 1, color); // add diagonally from center point.
+            SetPixel(hdc, x_point - 1, y_point - 1, color); // add inverted digonal from center point.
+            SetPixel(hdc, x_point, y_point - 1, color); // direct above center point.
+             SetPixel(hdc, x_point, y_point + 1, color); // direct below center point.
+            SetPixel(hdc, x_point - 1, y_point, color); // left side of center.
+            SetPixel(hdc, x_point + 1, y_point, color); // right side of center.
+            SetPixel(hdc, x_point - 1, y_point + 1, color); // left top from center
+            SetPixel(hdc, x_point + 1, y_point - 1, color); // right top
+           
+
+            SetPixel(hdc, enemy_x, enemy_y, 0x00000000);
             for (int line_top = 6; line_top < 326; line_top++) {
-                InvalidateRect(hWnd, NULL, FALSE);
                 SetPixel(hdc, line_top, 5, 0x00000000);
             }
             for (int line_bottom = 6; line_bottom < 326; line_bottom++) {
-                InvalidateRect(hWnd, NULL, FALSE);
                 SetPixel(hdc, line_bottom, 83, 0x00000000);
             }
             for (int line_left = 5; line_left < 84; line_left++) {
-                InvalidateRect(hWnd, NULL, FALSE);
                 SetPixel(hdc, 5, line_left, 0x00000000);
             }
             for (int line_right = 5; line_right < 84; line_right++) {
-                InvalidateRect(hWnd, NULL, FALSE);
                 SetPixel(hdc, 326, line_right, 0x00000000);
             }
             EndPaint(hWnd, &ps);
@@ -55,7 +65,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     wc.lpszClassName = mClassName;
     wc.hbrBackground = (HBRUSH) (COLOR_WINDOW);
     RegisterClassEx(&wc);
-    HWND hWnd = CreateWindowEx(0, mClassName, "Title", WS_OVERLAPPEDWINDOW, 740, 350, 348, 128, NULL, NULL, hInstance, NULL);
+    HWND hWnd = CreateWindowEx(0, mClassName, "Avoid Meteors!", WS_OVERLAPPEDWINDOW, 740, 350, 348, 128, NULL, NULL, hInstance, NULL);
     if(hWnd == NULL) return 0;
     ShowWindow(hWnd, nCmdShow);
 
@@ -64,34 +74,53 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     while(GetMessage(&Msg, NULL, 0, 0)) {
         TranslateMessage(&Msg);
         DispatchMessage(&Msg);
+
+        if (enemy_x >= 326) {
+            enemy_x = 0;
+        }
+        if (enemy_y >= 84) {
+            enemy_y = 0;
+        }
+        enemy_x++;
+        enemy_y++;
+        InvalidateRect(hWnd, NULL, TRUE);
+        SetPixel(hdc, enemy_x, enemy_y, 0x00000000);
+        
         // Get messages from keys in the main loop.
         if (GetAsyncKeyState(VK_LEFT)) {
             x_point = x_point - 1;
             InvalidateRect(hWnd, NULL, TRUE);
             SetPixel(hdc, x_point, y_point, 0x00000000);
-            UpdateWindow(hWnd);
         }
         if (GetAsyncKeyState(VK_RIGHT)) {
             x_point = x_point + 1;
             InvalidateRect(hWnd, NULL, TRUE);
             SetPixel(hdc, x_point, y_point, 0x00000000);
-            UpdateWindow(hWnd);
         }
         if (GetAsyncKeyState(VK_UP)) {
             y_point = y_point - 1;
             InvalidateRect(hWnd, NULL, TRUE);
             SetPixel(hdc, x_point, y_point, 0x00000000);
-            UpdateWindow(hWnd);
+ 
         }
         if (GetAsyncKeyState(VK_DOWN)) {
             y_point = y_point + 1;
             InvalidateRect(hWnd, NULL, TRUE);
             SetPixel(hdc, x_point, y_point, 0x00000000);
-            UpdateWindow(hWnd);
         }
-        
-    }
-    return Msg.wParam;  // returns messages ONCE after while.
 
+        if (enemy_x == x_point && enemy_y == y_point ||
+            x_point + 1, y_point + 1
+            x_point - 1, y_point - 1
+            ) {
+                InvalidateRect(hWnd, NULL, TRUE);
+                MessageBox(hWnd, "You're Hit!", "Score", MB_OK);
+                break;
+            
+        }
+
+    }
+
+    return Msg.wParam;  // returns messages ONCE after while.
 	return 0;
 }
