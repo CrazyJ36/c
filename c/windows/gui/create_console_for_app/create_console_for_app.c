74f6a4f45c0580a84e145c9992ab7790b6946aeb
CrazyJ36 <crazy_j36@hotmail.com>
2022-01-02 00:27:09 -0500

    windows: console output from window actions

--- c/windows/gui/create_console_for_app/create_console_for_app.c
+++ c/windows/gui/create_console_for_app/create_console_for_app.c
@@ -2,10 +2,23 @@
 #include <stdio.h>
 #include <io.h>
 
-#define CLICK1 000011
+// Random IDs for buttons.
+#define BTN1 1
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch(uMsg) {
+
+        // Get the clicked command from BTN1, which is initialized to the window in WinMain().
+        case WM_COMMAND:
+            switch(wParam) {
+                case(BTN1):
+                    // Setup A filestream to stdout for the open console and print to it.
+                    FILE *f;
+                    freopen_s(&f, "CONOUT$", "w", stdout); 
+                    printf("test printf on our console window.\n");
+                    return 0;
+            }
+
         case WM_CLOSE:
             DestroyWindow(hWnd); break;
         case WM_DESTROY:
@@ -26,17 +39,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     wc.hbrBackground = (HBRUSH) (COLOR_WINDOW + 1);
     wc.lpszClassName = mClassName;
     RegisterClassEx(&wc);
-
-    // Open/Attach A console to application
-    AllocConsole();
-
-    HWND hWnd = CreateWindowEx(0, mClassName, "App with Console", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 512, 248, NULL, NULL, hInstance, NULL);
-	
+    HWND hWnd = CreateWindowEx(0, mClassName, "App with Console", WS_OVERLAPPEDWINDOW, 248, 512, 348, 248, NULL, NULL, hInstance, NULL);
     if(hWnd == NULL) return 0;
 
+    // Attach A button to our apps' hWnd window.
+    HWND hWndButton1 = CreateWindow("BUTTON", "Button1", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 10, 10, 86, 32, hWnd, (HMENU)BTN1, (HINSTANCE)GetWindow(hWnd, GWL_EXSTYLE), NULL);
+    // Open/Attach A console to this application for use in WM_COMMAND in WndProc.
+    AllocConsole();
+    
     ShowWindow(hWnd, nCmdShow);
-
-	MSG Msg;
+    MSG Msg;
     while(GetMessage(&Msg, NULL, 0, 0)) {
         TranslateMessage(&Msg);
         DispatchMessage(&Msg);
