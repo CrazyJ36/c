d33de6ed7db5eb38fe2502f296717d507fe802d8
CrazyJ36 <crazy_j36@hotmail.com>
2022-01-27 05:08:41 -0500

    learned about EOF

---
+++ c/eof-value.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  /* An actual End-Of-File is -1,
+   less than zero characters. */
+  printf(
+    "EOF means: %d,\nless than zero characters\n",
+    EOF
+    );
+}
--- c/print-spaced-format.c
+++ c/print-spaced-format.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 int main() {
   printf("printing at columns:\n");
+
   printf("1\n");
-  // %2d specifies width of area to print to.
+
+  /* %2d specifies width of area to print to,
+     as right-justified, and to allow 2 spacess
+     that may need to be filled. */
   printf("%2d\n", 2);
+
   return 0;
 }
