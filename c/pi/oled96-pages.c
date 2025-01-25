a0b6853bd3cb8ad5d3166123c82ec67b28159819
CrazyJ36 <crazy_j36@hotmail.com>
2025-01-25 03:05:36 -0500

    u

--- c/pi/oled96-pages.c
+++ c/pi/oled96-pages.c
@@ -16,17 +16,24 @@ int main(int argc, char *argv[]) {
   oledInit(1, 0x3c, OLED_128x32, 0, 0);
   oledFill(0); // fill with black
 
-  // oledWriteString(x_pos, y_pos, string, size) is based on the start point of spaces for insertion
-  // ofcharacters, so x axis can be 0-15(16 characters display). Shows from left to right.
-  // y signifies 4 horizontal rows, 0-3 for normal font size..
-  // font sizes: FONT_SMALL - 8x8, FONT_BIG - 16x24, FONT_SMALL - 6x8,
-  //changing sizes can cause moving of characters into unwanted places.
-  oledWriteString(0, 0,"Visit", FONT_NORMAL);
-  oledWriteString(0, 1, "crazyj36.rocks", FONT_NORMAL);
-  oledWriteString(0, 2, "for New Age/Rock", FONT_NORMAL);
-  oledWriteString(0, 3, "Music", FONT_NORMAL);
+  // oledWriteString(x_pos, y_pos, string, size) is based on spaces for insertion
+  // of characters, so x axis can be 0-15(for 16 characters, on my Adafruit 128x32 oled display, using FONT_NORMAL).
+  // Shows from left to right.
+  // y signifies 4 horizontal rows, 0-3 for normal and small font size.
+  // space available in font sizes: FONT_NORMAL - 16x4, FONT_BIG - 8x1, FONT_SMALL - 21x4.
+  // const values of font sizes are: FONT_NORMAL - 0, FONT_BIG - 1, FONT_SMALL - 2.
+  oledWriteString(0, 0, "123456789012345678901", FONT_SMALL); // 21 characters in string.
+  oledWriteString(0, 1, "123456789012345678901", FONT_SMALL);
+  oledWriteString(0, 2, "123456789012345678901", FONT_SMALL);
+  oledWriteString(0, 3, "123456789012345678901", FONT_SMALL);
 
-  printf("Showning, press ENTER to turn off led and end program.\n");
+
+  printf("FONT_NORMAL const value: %d\n", FONT_NORMAL); // 0
+  printf("FONT_BIG const value: %d\n", FONT_BIG); // 1
+  printf("FONT_SMALL const value: %d\n", FONT_SMALL); // 2
+
+
+  printf("Showing on display, press ENTER to turn off led and end program.\n");
   getchar();
   oledShutdown();
   return 0;
