ce8c5f945dcf438265de9b62d5101fe2a1bce0a7
CrazyJ36 <crazy_j36@hotmail.com>
2025-01-25 04:30:15 -0500

    better code for my pi fan

--- c/pi/fan.c
+++ c/pi/fan.c
@@ -14,34 +14,33 @@ int main() {
   signal(SIGINT, inthand);
 
   FILE *fp;
-  char temp_celsius[8];
-  int temp_fahrenheit;
-  int conversion;
+  char buff[8];
+  int temp;
 
   if (!bcm2835_init()) { return 1; }
   bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
 
   while (!stop) {
+
     // Run system command to show temp, then check to see if it ran properly.
     fp = popen("/bin/cat /sys/class/thermal/thermal_zone0/temp", "r");
     if (fp == NULL) {
       printf("Failed to run command to get cpu temp.\n");
       return 1;
     }
-    // read the file output from the command and put it into char..
-    while (fgets(temp_celsius, sizeof(temp_celsius), fp) != NULL) {
 
-      // convert celcius to nice fahrenheit format using calculations.
-      temp_fahrenheit = atoi(temp_celsius) / 1000;
-      conversion = (temp_fahrenheit * 1.8) + 32;
+    // read the file output from the above command and put it into char buffer.
+    if (fgets(buff, sizeof(buff), fp) != NULL) {
+      // converting celcius output of new buffer to fahrenheit format using calculations.
+      temp = ((atoi(buff) / 1000) * 1.8) + 32;
       // print temp in fahrenheit, comment this out if you're running this program in background.
-      //printf("CPU Temperature in fahrenheit: %d\n", conversion);
+      printf("temperature in fahrenheit: %d\n", temp);
 
       // if fahrenheit temp is above 100, turn on fan
-      if (conversion > 99 && bcm2835_gpio_lev(LOW)) {
+      if (temp > 90 && bcm2835_gpio_lev(LOW)) {
         bcm2835_gpio_write(PIN, HIGH);
       }
-      else if (conversion <= 99 && bcm2835_gpio_lev(HIGH)) {
+      else if (temp <= 90 && bcm2835_gpio_lev(HIGH)) {
         bcm2835_gpio_write(PIN, LOW);
       }
     }
@@ -52,5 +51,4 @@ int main() {
   bcm2835_gpio_clr(PIN);
   bcm2835_close();
   return 0;
-
 }
